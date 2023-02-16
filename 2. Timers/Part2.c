8603cbc012a49afaeb00608c3309115c20442090
Caleb Amadoro <amador79@students.rowan.edu>
2023-02-16 18:22:40 -0500

    Part 2 working :)

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -1,13 +1,15 @@
 /*
  * Part2.c
  *
- *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *  Created on: Feb 16, 2023
+ *      Author: Caleb Amadoro
  *
  *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
  */
 
 #include <msp430.h>
+
+int delay = 20000;
 
 void gpioInit();
 void timerInit();
@@ -30,38 +32,50 @@ void main(){
 
 void gpioInit(){
     // @TODO Initialize the Red or Green LED
-
+    P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
     // @TODO Initialize Button 2.3
-
-
+    P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+    P2REN |= BIT3;                          // P2.3 pull-up register enable
+    P2IES |= BIT3;                          // P2.3 High --> Low edge
+    P2IE |= BIT3;                           // P2.3 interrupt enabled
 }
 
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
-
+    TB1CTL = ID_0 | TBSSEL_1 | MC_2;
+    TB1CCTL0 = CCIE;
+    TB1CCR0 = 50000;
 }
 
 
-/*
- * INTERRUPT ROUTINES
- */
+//INTERRUPT ROUTINES
 
 // Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-
+    P2IFG &= ~BIT3;
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
-
+    if (delay == 20000){
+        delay = 5000;
+    }
+    else if (delay == 5000){
+        delay = 1000;
+    }
+    else if (delay == 1000){
+        delay = 20000;
+    }
 }
-
 
 // Timer B1 interrupt service routine
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P6OUT ^= BIT6;
+    TB1CCR0 += delay;
 }
+
 
 
